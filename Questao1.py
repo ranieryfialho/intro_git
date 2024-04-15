c5409068f522a7cf90af50a52cfe4aff5f42d338
Raniery Fialho <ranieryfialho@gmail.com>
2024-04-15 23:10:08 +0000

    adição da primeira questão no repositório

---
+++ Questao1.py
@@ -0,0 +1,9 @@
+# Faça duas variáveis numéricas e descubra qual é a maior
+
+numero_um = int(input("Digite um número: "))
+numero_dois = int(input("Digite outro número: "))
+
+if numero_um > numero_dois:
+    print(f"O maior número é {numero_um}!")
+else:
+    print(f"O maior número é {numero_dois}")
