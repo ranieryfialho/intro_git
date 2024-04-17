b0e8fb188916f27fc8d3e113b1cecb492d644fbf
Raniery Fialho <ranieryfialho@gmail.com>
2024-04-17 23:02:34 +0000

    adição da atividade 4

---
+++ Atividade4.py
@@ -0,0 +1,8 @@
+# Crie uma função em python que receba um número diga se o número é par ou ímpar.
+
+numero = int(input("Digite um valor: "))
+
+if numero %2 == 0:
+    print(f"O número {numero} é par!")
+else:
+    print(f"O número {numero} é impar!")
